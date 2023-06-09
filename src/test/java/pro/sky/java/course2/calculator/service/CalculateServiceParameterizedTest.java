444668b5cd12a75e57050843bd58f9773910cb09
vpm789 <vpm789@gmail.com>
2023-06-09 14:16:29 +0300

    calculator_test

--- src/test/java/pro/sky/java/course2/calculator/service/CalculateServiceParameterizedTest.java
+++ src/test/java/pro/sky/java/course2/calculator/service/CalculateServiceParameterizedTest.java
@@ -13,38 +13,53 @@ public class CalculateServiceParameterizedTest {
     private final CalculateServiceImpl out = new CalculateServiceImpl();
 
     @ParameterizedTest
-    @MethodSource("provideParamsForTests")
+    @MethodSource("provideParamsForTestPlus")
     public void shouldReturnErrorWhenPlusIsIncorrect(int num1, int num2, int sum) {
         int result = out.calculatePlus(num1, num2);
         assertEquals(sum, result);
     }
 
     @ParameterizedTest
-    @MethodSource("provideParamsForTests")
+    @MethodSource("provideParamsForTestMinus")
     public void shouldReturnErrorWhenMinusIsIncorrect(int num1, int num2, int difference) {
         int result = out.calculateMinus(num1, num2);
         assertEquals(difference, result);
     }
 
     @ParameterizedTest
-    @MethodSource("provideParamsForTests")
+    @MethodSource("provideParamsForTestMultiply")
     public void shouldReturnErrorWhenMultiplyIsIncorrect(int num1, int num2, int multiplication) {
         int result = out.calculateMultiply(num1, num2);
         assertEquals(multiplication, result);
     }
 
     @ParameterizedTest
-    @MethodSource("provideParamsForTests")
+    @MethodSource("provideParamsForTestDivide")
     public void shouldReturnErrorWhenDivideIsIncorrect(int num1, int num2, int division) {
         int result = out.calculateDivide(num1, num2);
         assertEquals(division , result);
     }
-    public static Stream<Arguments> provideParamsForTests() {
+    public static Stream<Arguments> provideParamsForTestPlus() {
         return Stream.of(
-                Arguments.of(NUM1, NUM2, CORRECT_PLUS),
-                Arguments.of(NUM1, NUM2, CORRECT_MINUS),
-                Arguments.of(NUM1, NUM2, CORRECT_MULTIPLY),
+                Arguments.of(NUM1, NUM2, CORRECT_PLUS)
+        );
+    }
+
+    public static Stream<Arguments> provideParamsForTestDivide() {
+        return Stream.of(
                 Arguments.of(NUM1, NUM2, CORRECT_DIVIDE)
+        );
+    }
+
+    public static Stream<Arguments> provideParamsForTestMinus() {
+        return Stream.of(
+                Arguments.of(NUM1, NUM2, CORRECT_MINUS)
+        );
+    }
+
+    public static Stream<Arguments> provideParamsForTestMultiply() {
+        return Stream.of(
+                Arguments.of(NUM1, NUM2, CORRECT_MULTIPLY)
         );
     }
 }
